6aee16b2e0de88325b6136c38bce8201f433993f
Darlles Almeida <darllesalmeida37@gmail.com>
2025-07-26 10:58:27 -0300

    Adicionei a imagem de fundo da página e fiz algumas alterações no javascript.
    Adicionei uma nova imagem de fundo na página.
    Fiz também algumas alterações no javascript, adicionei todas as imagens conforme a página vai sendo interagida pelo usuário.
    No final, fiz alguns ajustes no CSS, troquei algumas cores e tamanhos de fontes.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # declaracao-dia-dos-namorados
 Criei uma página interativa do dias dos namorados para a minha esposa. Não tenho certeza se usei as boas práticas de programação, mas eu tentei fazer com os conhecimentos que eu tenho atualmente.
 
-A página vai iniciar pedindo a data(dia, mês e ano) do inicio do namoro, se o usuário acertar a data, o sistema mostrará na tela uma sequência de mensagens e fotos do casal.
+A página vai iniciar pedindo a data(dia 28, mês 10, e ano 2014) do inicio do namoro, se o usuário acertar a data, o sistema mostrará na tela uma sequência de mensagens e fotos do casal.
 
 Lembrando, eu fiz a página para a minha esposa, logo, a data e as fotos que eu adicionei com Javascript, são nossas.
--- pagina-interativa-declaracao/script.js
+++ pagina-interativa-declaracao/script.js
@@ -5,9 +5,9 @@ function verificar(){
   let text1 = document.getElementById('text1')
   let pdata = document.getElementById('pdata')
 
-  if(dataEsc === '2025-07-25'){
+  if(dataEsc === '2014-10-28'){
     alert(`PARABEÉNS VOCÊ ACERTOOU!  Aperte "OK" Para Continuar!`)
-    foto1.src = 'imagens/foto1.png' // falta alterar todas as fotos para EX: "imagens/foto2.png" E "imagens/foto3.png" E "imagens/foto4.png"
+    foto1.src = 'imagens/foto1.png'
     text1.innerHTML = `<p>28/10/2014 - Foi onde tudo começou pra gente! 
     Foi quando decidimos que apartir dalí, andaríamos juntos, Lado a Lado!</p>`
 
@@ -26,7 +26,7 @@ function verificar(){
 
     function continuar(){
         if(foto1 && text1){
-            foto1.src = 'foto2.png'
+            foto1.src = 'imagens/foto2.png'
             text1.innerHTML = `<p>Foram muitos momentos que passamos juntos, momentos surreais; momentos ótimos; momentos bons e alguns ruins também! 
         Momentos esses que nos fizeram ser quem somos hoje! Eu, particularmente não me arrependo de nada 
         (Você deve está se perguntando: como assim não se arrepende? Errei muitas vezes, Te fiz chorar várias vezes!). Errei muito, isso é verdade, mas os mesmos erros que TE fizeram CHORAR, ME fizeram MUDAR! 
@@ -51,7 +51,7 @@ function verificar(){
     function proximo(){
         let bloco3 = document.getElementById('bloco3')
         if(foto1 && text1){
-            foto1.src = 'foto3.png'
+            foto1.src = 'imagens/foto3.png'
             text1.innerHTML = `<p> Que a cada dia nosso amor se renove, que a cada briga possamos amadurecer, 
             que a cada obstáculo da vida possamos ficar mais fortes! Deus fez VOCÊ pra MIM, e ME fez para VOCÊ, 
             fomos unidos por ELE em uma só carne. E se eu pudesse escolher de novo, SEMPRE escolheria VOCÊ!</p>`
@@ -75,12 +75,11 @@ function verificar(){
         let bloco4 = document.getElementById('bloco4')
         if(foto1 && text1){
             text1.innerHTML = `<p>I LOVE YOU!</P>`
-            foto1.src = 'foto4.png'
+            foto1.src = 'imagens/foto4.png'
             text1.style.fontSize = '100px'
             text1.style.color = 'red'
             text1.style.fontFamily = 'san-serif'
             text1.style.fontWeight = '600'
-            text1.style.boxShadow = '-2px -2px 10px #ffffffc2'
 
         }
         if(bloco4){
